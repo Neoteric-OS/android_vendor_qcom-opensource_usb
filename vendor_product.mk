f32b83aaa20e678dae815304a2e9d2305bc8574e
Krishna Kurapati <quic_kriskura@quicinc.com>
2022-03-16 11:07:48 +0530

    USB: Enable USBGadget HAL for Neo
    Enable USB Gadget HAL on Neo platform.
    
    Change-Id: Ie972bb34df8f7a8819885397d6fa1e8fd01e4f2f

--- vendor_product.mk
+++ vendor_product.mk
@@ -61,7 +61,7 @@ else
   # USB Gadget HAL is enabled on newer targets and takes the place
   # of the init-based configfs rules for setting USB compositions
   #
-  ifneq ($(filter seraph niobe taro kalama pineapple sun parrot blair monaco pitti volcano anorak61 neo61,$(TARGET_BOARD_PLATFORM)),)
+  ifneq ($(filter seraph niobe taro neo kalama pineapple sun parrot blair monaco pitti volcano anorak61 neo61,$(TARGET_BOARD_PLATFORM)),)
     PRODUCT_PROPERTY_OVERRIDES += vendor.usb.use_gadget_hal=1
     PRODUCT_PACKAGES += android.hardware.usb.gadget-service.qti
     PRODUCT_PACKAGES += usb_compositions.conf
